bbed5b1cddd4ea0c7b325753a7763526a74b1d63
ABERYUF <benedictjr87@gmail.com>
2025-10-17 09:49:20 +0100

    Update bot settings and owner number

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
+  packname: 'BMEDIA BOTLINE-MD',
+  author: '‎BMEDIA',
   botName: "Knight Bot",
   botOwner: 'BMEDIA', // Your name
-  ownerNumber: '237689660487', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '237679261475', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
